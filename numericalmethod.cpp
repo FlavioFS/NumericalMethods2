a3dea999457958ace8030a62eb262861b992fcce
FlavioFS <flaviofreitas.h@gmail.com>
2014-02-18 14:41:13 -0300

    Created: numericalmethod.cpp, numericalmethod.h

---
+++ numericalmethod.cpp
@@ -0,0 +1,5 @@
+#include "numericalmethod.h"
+
+NumericalMethod::NumericalMethod()
+{
+}
---
+++ numericalmethod.h
@@ -0,0 +1,10 @@
+#ifndef NUMERICALMETHOD_H
+#define NUMERICALMETHOD_H
+
+class NumericalMethod
+{
+public:
+    NumericalMethod();
+};
+
+#endif // NUMERICALMETHOD_H
