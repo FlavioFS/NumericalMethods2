83b525d007d3bab624ca82f7633e4587f7686a41
inafalcao <inafalcao@gmail.com>
2014-03-06 20:21:49 -0300

    Calling all the methods until now.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include "Sample.h"
 #include "Trapezoidal.h"
 #include "Simpsons38.h"
+#include "Simpsons13.h"
 
 using namespace std;
 
@@ -24,11 +25,19 @@ int main()
     // In the future, call shoud be like this:
     // IMethod *method =  new Trapezoidal(sample->getSamples());
 
-    //Trapezoidal method (sample->getSamples());
-    Simpsons38 method (sample->getSamples());
-    method.run();
-    // TOO put this on the view logic
-    cout << endl << "Area: " << method.getArea();
+    Trapezoidal trapezoidal (sample->getSamples());
+    trapezoidal.run();
+    
+    Simpsons13 simpsom13 (sample->getSamples());
+    simpsom13.run();
+
+    Simpsons38 simpson38 (sample->getSamples());
+    simpson38.run();
+
+    // TODO put this on the view logic
+    cout << endl << "Area| Trapezio: " << trapezoidal.getArea() << "\n";
+    cout << endl << "Area| 1/3 Simpsom: " << simpsom13.getArea() << "\n";
+    cout << endl << "Area| 3/8 Simpsom: " << simpson38.getArea() << "\n";
 
     return 0;
 }
