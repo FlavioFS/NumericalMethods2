e3c78fd059c72e74cf7b2903d46dc2f1699bf8ed
Henrique Siqueira Araújo <henrique.araujo88@gmail.com>
2014-05-01 13:34:45 -0300

    Update QrHouseholder.cpp
    Added comments, fixed a .put used wrongly, fixed variable typo.

--- src/methods/qr/householder/QrHouseholder.cpp
+++ src/methods/qr/householder/QrHouseholder.cpp
@@ -57,11 +57,11 @@ bool Qr::run()
 
 /*-----------------------Householder--------------------*/
 
-   for (int j = 1; j < order-1; j++)
+   for (int j = 1; j < order-3; j++) //Column loop goes from the first column until the order-2 one
    {
-	int s = 0;
+	int s = 0; // the sqrt of the sum of each column element
 	
-	for (int i = j+1; i < order; i++)
+	for (int i = j+1; i < order; i++) // line loop for s checks every single line below the main diagonal
 	{
 	   double aux;
 	   aux = A.get(i,j);
@@ -69,23 +69,23 @@ bool Qr::run()
 	}
 	s = sqrt(s);
 
-	Matrix C(order,1);
+	Matrix C(order,1); // Instantiates the vertical vector C
 	for (int k = 0; k < order; k++)
 	{
-	   if (k <= j)
+	   if (k <= j) //If k is below or equal the main diagonal, place 0 to vector C
 		{
-		   C.put(0,i+1,j+1);
+		   C.put(0,k+1,1);
 		}
-	   else if (k = j+1)
+	   else if (k = j+1) //If it is one unity above it...
 		{
 		  double signal_aux = A.get(k+1,j+1);
-		  if (signal_aux < 0)
+		  if (signal_aux < 0) //...if it's negative, signal is negative...
 		  {
 		     C.put(signal_aux - s,k+1,1);
 		  }
-		  else
+		  else //... and if it's positive, signal is positive.
 		  {
-		     C.put(signal_aux + s,k+1,1);
+		     C.put(signal_aux + s,k+1,1); 
 		  }
 		}
 	  else
@@ -94,11 +94,11 @@ bool Qr::run()
 	  }
 	}
 
-	Matrix ctc = C.transpose()*C;
-	double ctc_value = 1/sqrt(ctc.get(1,1));
+	Matrix ctc = C.transpose()*C; //getting the value of c.transpose*c. Since it'll generate a matrix [1.1]...
+	double ctc_value = 1/sqrt(ctc.get(1,1)); //... we use these shennaningans to extract it from the matrix.
 	Matrix e = C*ctc_value;
 	
-	Matrix I;
+	Matrix I; //now we declare an Identity matrix for using later.
 	for(int i = 0; i < order; i++)
 	{
 		for(int j = 0; j < order; j++)
@@ -115,7 +115,7 @@ bool Qr::run()
 	}
 
 	
-	Matrix Q = I - e*e.transpose()*2;
+	Matrix Q = I - e*e.transpose()*2; //and at this time, it's pretty much applying transformations.
 	R = Q*R*Q.transpose();
    }
 
