f56f1a2486d376387cbfc852bb4cdf4f22f4c3cf
inafalcao <inafalcao@gmail.com>
2014-05-18 01:06:56 -0300

    Third Order Runge Kutta added.

--- src/methods/runge-kutta/RungeKutta.h
+++ src/methods/runge-kutta/RungeKutta.h
@@ -11,6 +11,9 @@ class RungeKutta
         // [1] - Second Order Runge Kutta
         static double secondOrder (FunctionRn f, double v0, double h, double left, double right);
 
+        // [2] - Third Order Runge Kutta
+		static double thirdOrder (FunctionRn f, double v0, double h, double left, double right);
+
 };
 
 #endif // RUNGE_KUTTA_H
