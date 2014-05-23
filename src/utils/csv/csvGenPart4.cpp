51766e5ebeeb7eeefe6b057df8c651376fcbfb16
inafalcao <inafalcao@gmail.com>
2014-05-22 23:56:44 -0300

    Complete csv table for one function choice.

--- src/utils/csv/csvGenPart4.cpp
+++ src/utils/csv/csvGenPart4.cpp
@@ -6,11 +6,20 @@
 #include "../../methods/runge-kutta/RungeKutta.h"
 #include "../../methods/adams/Adams.h"
 #include "../../common/sample-initial-value.h"
+#include "../../common/Function.h"
 
 using namespace std;
 
 
-void csvEntries();
+static double f1 (double x);
+static double f2 (double x);
+static double f3 (double x);
+
+double f1 (double x) { return exp( 0.25*pow(x, 4) - 1.5*x  ); }
+// + ou - ?
+double f2 (double x) { return 1/4 * pow( (2*pow(x, 2) + x + 2), 2 ); }
+double f3 (double x) { return 1/4 * (2*pow(x, 2) - 2*x + 3*exp(-2*x) + 1); }
+
 
 int main(int argc, char *argv[])
 {
@@ -20,6 +29,7 @@ int main(int argc, char *argv[])
     sampleIV->readSamplesFromFile(argv[1]);
     
     // Vectors to store the results we want to compare
+    std::vector<point> resultsEuler;
     std::vector<point> resultsRK2;
     std::vector<point> resultsRK3;
     std::vector<point> resultsRK4;
@@ -29,12 +39,20 @@ int main(int argc, char *argv[])
     // Starting CVS
     ofstream csv ("src/utils/csv/table.csv"); 
     
-    csv << "-, Solution, RK 2,  RK 3, RK 4, Adams 3, Adams 4\n";
+    csv << "-, Solution, Euler, RK 2,  RK 3, RK 4, Adams 3, Adams 4\n";
     csv << "ti, f(ti) \n";
 
 
     // Storing results
-    resultsRK2.clear();
+
+    resultsEuler = RungeKutta::firstOrder (
+        sampleIV->getFunction(), 
+        sampleIV->getV0(), 
+        sampleIV->getH(), 
+        sampleIV->getLeft(), 
+        sampleIV->getRight()
+    );
+
     resultsRK2 = RungeKutta::secondOrder (
         sampleIV->getFunction(), 
         sampleIV->getV0(), 
@@ -42,7 +60,15 @@ int main(int argc, char *argv[])
         sampleIV->getLeft(), 
         sampleIV->getRight()
     );
-resultsRK4.clear();
+
+    resultsRK3 = RungeKutta::thirdOrder (
+        sampleIV->getFunction(), 
+        sampleIV->getV0(), 
+        sampleIV->getH(), 
+        sampleIV->getLeft(), 
+        sampleIV->getRight()
+    );
+
     resultsRK4 = RungeKutta::fourthOrder (
         sampleIV->getFunction(), 
         sampleIV->getV0(), 
@@ -69,16 +95,26 @@ resultsRK4.clear();
 
     // ---------------------------------------------------
 
+
+    Function f;
+    f.pushBackFunction(f1);
+    f.pushBackFunction(f2);
+    f.pushBackFunction(f3);
+    f.pickFunction(sampleIV->getFunctionID());
+
+
     for (int i = 0; i < resultsRK2.size(); i++)
     {
-        csv << resultsRK2[i].x    << ", " 
+        csv    << resultsA4[i].x    << ", " 
+               << f.run(resultsRK2[i].x) << ", " 
+               << resultsEuler[i].y << ", " 
                << resultsRK2[i].y << ", " 
                << resultsRK3[i].y << ", " 
                << resultsRK4[i].y << ", " 
                << resultsA3[i].y  << ", " 
                << resultsA4[i].y  << "\n";
     }
-    
+
     csv.close();
 
     return 0;
